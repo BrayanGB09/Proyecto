c0d06020c48a408108fc349964fa90c195b73076
BrayanGB09 <bgutierrez@fwdcostarica.com>
2024-07-24 16:05:38 -0600

    cambios

---
+++ src/js/proyecto.js
@@ -0,0 +1,11 @@
+const sesion = document.getElementById("sesion")
+const contraseña = document.getElementById("contraseña")
+const btnI = document.getElementById("btnI")
+const btnC = document.getElementById("btnC")
+
+btnI.addEventListener("click", function () {
+    
+
+
+})
+
---
+++ src/js/registro.js
@@ -0,0 +1,5 @@
+const hombre = document.getElementById("hombre")
+const mujer = document.getElementById("mujer")
+const otro = document.getElementById("otro")
+
+
